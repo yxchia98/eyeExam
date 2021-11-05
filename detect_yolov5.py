75310957489c4c765b478f6346767cfd98275c4a
Yi Xuan <yxchia98@gmail.com>
2021-11-05 16:01:49 +0800

    upate

---
+++ detect_yolov5.py
@@ -0,0 +1,47 @@
+import torch
+import numpy as np
+import cv2
+import pafy
+from time import time
+
+
+
+if __name__ == '__main__':
+    # model = torch.hub.load('ultralytics/yolov5', 'yolov5s6', pretrained=True)
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path='./weights/crowdhuman_yolov5m.pt')
+    # model = torch.hub.load('ultralytics/yolov5', 'custom', path='./weights/body1500_yolov5s6.pt')
+    model.classes = [0]
+    classes = model.names
+    cap = cv2.VideoCapture(0) # use the current device's default camera (webcam on laptops) as video resource
+    # cap = cv2.VideoCapture('./videos/people_walking.mp4') # capture a video in local directory
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model.to(device)
+
+    while cap.isOpened():
+        _, frame = cap.read()
+        count = 0
+
+        # Score frames
+        frames = [frame]
+        results = model(frames)
+        labels, cord = results.xyxyn[0][:, -1].numpy(), results.xyxyn[0][:, :-1].numpy()
+        unique, counts = np.unique(labels, return_counts=True)
+        print(dict(zip(unique, counts)))
+
+        # Plot boxes
+        n = len(labels)
+        x_shape, y_shape = frame.shape[1], frame.shape[0]
+        for i in range(n):
+            row = cord[i]
+            if row[4] >= 0.2:
+                x1, y1, x2, y2 = int(row[0]*x_shape), int(row[1]*y_shape), int(row[2]*x_shape), int(row[3]*y_shape)
+                bgr = (0, 140, 255)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 1)
+                cv2.putText(frame, classes[int(labels[i])], (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, bgr, 1)
+
+        cv2.imshow('Webcam', frame)
+
+
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 import cv2
+import datetime
 import sys
 import numpy as np
-
-
-
+import pafy
 
 
 if __name__ == '__main__':
-    s = 0
-    if len(sys.argv) > 1:
-        s = sys.argv[1]
+    # url = "https://www.youtube.com/watch?v=IzbCLBXAGFM"
+    # video = pafy.new(url)
+    # best = video.getbest(preftype="mp4")
+    # face_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')   # haarcascade fullbody model
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')   # haarcascade face model
+    # cap = cv2.VideoCapture('./videos/people_walking.mp4') # capture a video in local directory
+    cap = cv2.VideoCapture(0) # use the current device's default camera (webcam on laptops) as video resource
 
-    source = cv2.VideoCapture(s)
+    while cap.isOpened():
+        _, frame = cap.read()
+        count = 0
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.1, 1)
+        for (x, y, w, h) in faces:
+            count += 1
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 1)
 
-    win_name = 'Camera Preview'
-
-    cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
-
-    while cv2.waitKey(1) != 27: # esc
-        has_frame, frame = source.read()
-        if not has_frame:
+        cv2.putText(frame, 'Count:' + str(count), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 1, cv2.LINE_AA)
+        cv2.imshow('Webcam', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-        cv2.imshow(win_name, frame)
 
-    source.release()
-    cv2.destroyWindow(win_name)
+
+
+
+
